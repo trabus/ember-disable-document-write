794f0f3afece501c07bcd756c17dbc8cd2753834
Jake Bixby <trabus@gmail.com>
2016-11-30 18:46:41 -0800

    we don't necessarily need the init in the document service blueprint, generating it commented out now

--- blueprints/document-service/files/app/services/document.js
+++ blueprints/document-service/files/app/services/document.js
@@ -1,9 +1,11 @@
 import DocumentService from 'ember-disable-document-write/services/document';
 
 export default DocumentService.extend({
+    /*
     init() {
         this._super(...arguments);
     },
+    */
     write(/* content */) {
         // insert any functionality to be executed when document.write is called
     }
